c84a46eee9273b5ffeced5213a7c8d56a53cbb1f
NicoPiovano <npiovano@sugit.com.ar>
2026-02-13 18:01:25 -0300

    Port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: { port: 5171 },
   appType: 'spa',
   preview: {
     strict: false, // sirve index.html para rutas como /login (evita 404)
